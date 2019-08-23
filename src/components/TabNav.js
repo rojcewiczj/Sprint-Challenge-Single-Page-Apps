c92ae323412574a5fb6eae5954a35027fe52372a
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-23 10:16:07 -0500

    got some tab styling

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -6,6 +6,23 @@ import { NavLink } from "react-router-dom";
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
-
+     const panes = [
+        { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+        { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+        { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+      ]
+      
+      const TabExampleBasic = () => <Tab panes={panes} />
+return (
+   
+    <div>
+  <div class="ui attached tabular menu">
+    <a class="active item">Tab 1</a>
+    <a class="item">Tab 2</a>
+    <a class="item">Tab 3</a>
+  </div>
+  <div class="ui bottom attached segment active tab">Tab 1 Content</div>
+</div>
+)
 };
 
